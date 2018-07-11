eaf233615e05d4ab9659aa8144965b890c6c8091
Bruno Dutra <brunordutra@gmail.com>
2018-07-11 09:46:50 -0300

    Ajustes importações

--- src/main/java/com/telefonica/efikaauth/controller/AuthController.java
+++ src/main/java/com/telefonica/efikaauth/controller/AuthController.java
@@ -1,11 +1,6 @@
 package com.telefonica.efikaauth.controller;
-import com.telefonica.efikaauth.application.Greeting;
-import com.telefonica.efikaauth.model.UsuarioModel;
-import java.util.concurrent.atomic.AtomicLong;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
